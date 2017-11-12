f9871b2d8344d5f800df9cccb2c5306c86873255
asyks <aaron@aaronsykes.com>
2017-11-11 19:47:47 -0800

    Preliminary websocket connection working

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 require('dotenv').config()
 const express = require('express')
 const request = require('request')
+var WebSocket = require('websocket').w3cwebsocket
 
 // Constants
 const PORT = 8080
@@ -24,6 +25,10 @@ app.get('/', function(req, res) {
     res.json(payload)
 })
 
+// bot token
+var bot_token = null
+var websocket_url = null
+
 app.get('/another-route', function(req, res) {
     var payload = {'jsonkey': 'jsonvalue', 'jsonkey2': 2}
     console.log('Payload: ', payload)
@@ -33,6 +38,40 @@ app.get('/another-route', function(req, res) {
 app.get('/auth', function(req, res) {
     res.sendFile(__dirname + '/add_to_slack.html')
 })
+
+function connect_websocket(error, response, body) {
+    var JSONResponse = JSON.parse(body)
+    websocket_url = JSONResponse.url
+
+    var ws = new WebSocket(websocket_url);
+
+    ws.onopen = function() {
+        console.log('WebSocket Client Connected')
+    }
+
+    ws.onerror = function() {
+        console.log('Connection Error')
+    }
+
+    ws.onmessage = function(message) {
+        if (typeof message.data === 'string') {
+            console.log("Received: '" + message.data + "'");
+        }
+    }
+
+}
+
+function send_rtm_connect_request(token) {
+    var options = {
+        uri: 'https://slack.com/api/rtm.connect',
+        form: {
+            'token': token
+        }
+    }
+
+    request.post(options, connect_websocket)
+}
+
 
 app.get('/auth/redirect', function(req, res) {
     var options = {
@@ -44,7 +83,7 @@ app.get('/auth/redirect', function(req, res) {
         method: 'GET'
     }
 
-    function callback(error, response, body) {
+    function main_auth_callback(error, response, body) {
         var JSONresponse = JSON.parse(body)
 
         if (!JSONresponse.ok) {
@@ -53,21 +92,15 @@ app.get('/auth/redirect', function(req, res) {
             ).status(200).end()
         }
         else {
-            var options = {
-                uri: 'https://slack.com/api/rtm.connect',
-                form: {
-                    'token': JSONresponse.bot.bot_access_token
-                }
-            }
             res.send("Success!")
             console.log(JSONresponse)
-            request.post(options, function(error, response, body) {
-                console.log(JSON.parse(body))
-            })
+
+            bot_token = JSONresponse.bot.bot_access_token
+            send_rtm_connect_request(bot_token)
         }
     }
 
-    request(options, callback)
+    request(options, main_auth_callback)
 
 })
 
