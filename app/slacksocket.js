05250efb6c53bbd8f7dbdcadaefe99196dc96ab7
asyks <aaron@aaronsykes.com>
2018-01-28 12:08:29 -0800

    Basic socket message parsing, and duckduckgo query working

--- app/slacksocket.js
+++ app/slacksocket.js
@@ -7,6 +7,19 @@ var websocket_url = null
 const { Requester } = require("node-duckduckgo");
 const requester = new Requester("node-slackipedia");
 
+function create_response_payload(msg_channel, msg_text) {
+    return JSON.stringify(
+        {
+            "id": 1,
+            "type": "message",
+            "channel": msg_channel,
+            "text": "message recieved! '{msg_text}'".replace(
+                "{msg_text}", msg_text
+            )
+        }
+    )
+}
+
 function connect_websocket(error, response, body) {
     var JSONResponse = JSON.parse(body)
     websocket_url = JSONResponse.url
@@ -22,30 +35,43 @@ function connect_websocket(error, response, body) {
     }
 
     ws.onmessage = function(message) {
-        if (typeof message.data === 'string') {
-            console.log("Received: '" + message.data + "'");
-            var msg_data = JSON.parse(message.data)
-            if (typeof msg_data.type === 'string' && msg_data.type === 'message') {
-                console.log(msg_data.text)
-                ws.send(
-                    JSON.stringify({
-                        "id": 1,
-                        "type": "message",
-                        "channel": msg_data.channel,
-                        "text": "message recieved!"
-                    })
-                )
 
-                requester.request(msg_data.text, (err, response, body) => {
-                    if (err) {
-                        console.log(err);
-                        return;
-                    }
-                    console.log(body);
-                });
-            }
+        console.log("message recieved")
+        
+        if (typeof message.data === 'string') {
+            var msg_data = JSON.parse(message.data)
+
+            console.log("Received: '" + msg_data + "'")
+        }
+
+        if (
+            typeof msg_data.type === 'string' && msg_data.type === 'message'
+        ) {
+            var msg_channel = msg_data.channel
+            var msg_text = msg_data.text.slice(13)
+
+            send_duck_duck_go_request(msg_text)
+
+            var response_payload = create_response_payload(
+                msg_channel, msg_text
+            )
+
+            ws.send(response_payload)
         }
     }
+}
+
+function send_duck_duck_go_request(msg_data) {
+
+    requester.request(msg_data.text)
+        .on("data", (data) => {
+            console.log("Data recieved");
+            console.log(data.toString());
+        })
+        .on("error", (err) => {
+            console.log("ERROR!");
+            console.log(err);
+        });
 }
 
 module.exports = connect_websocket
