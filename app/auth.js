fa6a2b1dfcd7a467850e16e4e6a05cf401f0d020
Aaron Sykes <aaron@aaronsykes.com>
2018-03-11 22:26:11 -0700

    Standard JS style changes (#6)
    * Made linting changes to: server.js, auth.js, duckduckgoer.js, duckduckgoer.js

--- app/auth.js
+++ app/auth.js
@@ -5,31 +5,31 @@ const req = require('request-promise')
 module.exports.authOptions = authOptions
 module.exports.rtmConnect = rtmConnect
 
-function authOptions(queryCode) {
-    return {
-        method: 'POST',
-        uri: 'https://slack.com/api/oauth.access?code='
-            + queryCode +
-            '&client_id='+ process.env.CLIENT_ID+
-            '&client_secret='+ process.env.CLIENT_SECRET+
-            '&redirect_uri='+ process.env.REDIRECT_URI,
-        json: true
-    }
+function authOptions (queryCode) {
+  return {
+    method: 'POST',
+    uri: 'https://slack.com/api/oauth.access?code=' +
+      queryCode +
+      '&client_id=' + process.env.CLIENT_ID +
+      '&client_secret=' + process.env.CLIENT_SECRET +
+      '&redirect_uri=' + process.env.REDIRECT_URI,
+    json: true
+  }
 }
 
-function rtmAuthOptions(token) {
-    return {
-        method: 'POST',
-        uri: 'https://slack.com/api/rtm.connect',
-        form: {
-            'token': token
-        },
-        json: true
-    }
+function rtmAuthOptions (token) {
+  return {
+    method: 'POST',
+    uri: 'https://slack.com/api/rtm.connect',
+    form: {
+      'token': token
+    },
+    json: true
+  }
 }
 
-function rtmConnect(token) {
-    var options = rtmAuthOptions(token)
+function rtmConnect (token) {
+  var options = rtmAuthOptions(token)
 
-    return req(options)
+  return req(options)
 }
--- app/duckduckgoer.js
+++ app/duckduckgoer.js
@@ -1,28 +1,27 @@
 'use strict'
 
-const { Requester } = require("node-duckduckgo")
+const { Requester } = require('node-duckduckgo')
+
 const slacksocket = require('./slacksocket')
 
 module.exports = sendDuckRequest
 
-const requester = new Requester("node-slackduck")
+const requester = new Requester('node-slackduck')
 requester.no_html = 1
 
-function sendDuckRequest(webSocket, msgChannel, queryTerm) {
+function sendDuckRequest (webSocket, msgChannel, queryTerm) {
+  requester.request(queryTerm, function (err, response, body) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('DuckDuckGo response recieved')
 
-    requester.request(queryTerm, function(err, response, body) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            console.log("DuckDuckGo response recieved")
+      var jsonBody = JSON.parse(body)
 
-            var jsonBody = JSON.parse(body)
-
-            var respPayload = slacksocket.responsePayload(
-                msgChannel, jsonBody.AbstractURL
-            )
-            webSocket.send(respPayload)
-        }
-    })
+      var respPayload = slacksocket.responsePayload(
+        msgChannel, jsonBody.AbstractURL
+      )
+      webSocket.send(respPayload)
+    }
+  })
 }
